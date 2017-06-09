69e52477be6d1e7c3ff6a7a511f1b57cd1cd2850
Diego Vogogna <diego.vogogna@tech-value.com>
2017-06-09 15:27:42 +0200

    Read config file if available or create a new one

--- winelauncher/configfile.py
+++ winelauncher/configfile.py
@@ -1,4 +1,6 @@
 import configparser
+import pathlib
+import sys
 
 from xdg.BaseDirectory import xdg_config_home, xdg_data_home
 
@@ -17,3 +19,18 @@ config['logging'] = {
     "log_dest": "syslog",
     "log_level": "info"
 }
+
+
+def init_config(config_file):
+    """ Check if config file exists, otherwise generate a default one """
+    if pathlib.Path(config_file) and config.read(config_file):
+        print("Using configuration from {}".format(config_file))
+    else:
+        try:
+            with open(config_file, mode="w") as newfile:
+                config.write(newfile)
+            print("Saved default config file {}".format(config_file))
+            sys.exit(0)
+        except OSError as err:
+            print("Cannot open file {}".format(config_file))
+            print("OSError: {0}".format(err))
