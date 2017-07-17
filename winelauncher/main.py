f58220c79ad7edb0294f7a3a5f133ab644cdebc4
Diego Vogogna <diego.vogogna@tech-value.com>
2017-07-17 17:36:44 +0200

    setup.py

--- winelauncher/main.py
+++ winelauncher/main.py
@@ -95,6 +95,9 @@ def main():
     wine_env['NINEDEBUG'] = os.environ.get(
         'NINEDEBUG', lookup(config, config_section, 'nine_debug'))
 
+    # Fix for ugly fonts
+    wine_env['FREETYPE_PROPERTIES'] = "truetype:interpreter-version=35"
+
     log.info('Enviroment: {}'.format(wine_env))
     wine_exec = args.winecommand
     if wine_exec[0] == 'winetricks':
